1115e48b81d32ce43a67a410ff7396f44465181e
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-09-26 16:11:29 +0200

    Task Done

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
@@ -4,6 +4,29 @@ class S4532 extends StatelessWidget {
   const S4532({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MiStatelessWidget();
+  }
+}
+
+class MiStatelessWidget extends StatelessWidget {
+  const MiStatelessWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("App Akademie"),
+        Text(
+          "App Akademie",
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold, color: Colors.blue),
+        ),
+        Text(
+          "App Akademie",
+          style: TextStyle(
+              fontSize: 10, fontStyle: FontStyle.italic, color: Colors.green),
+        ),
+      ],
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -2,8 +2,35 @@ import 'package:flutter/material.dart';
 
 class S4536 extends StatelessWidget {
   const S4536({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        final product = products[index];
+        return ListTile(
+          leading: const Icon(Icons.shopping_cart),
+          title: Text(product.name),
+          subtitle: Text("Preis: €${product.price.toStringAsFixed(2)}"),
+        );
+      },
+    );
   }
 }
+
+class Product {
+  final String name;
+  final double price;
+
+  Product({required this.name, required this.price});
+}
+
+final List<Product> products = [
+  Product(name: "Seife", price: 29.99),
+  Product(name: "Schokolade", price: 49.99),
+  Product(name: "Vanile", price: 19.99),
+  Product(name: "Shampoo", price: 89.99),
+  Product(name: "Burger", price: 39.99),
+];
